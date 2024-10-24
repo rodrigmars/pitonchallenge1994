d5b8b50814147976d0907c8696fac812bc2ee989
rodrigmars <rodrigmars@gmail.com>
2024-10-24 09:54:36 -0300

    Otimizando código

--- challenge1994/app.py
+++ challenge1994/app.py
@@ -1,56 +1,58 @@
-def display_message(message: list[dict]) -> None:
 
-    for entry in sorted(message, key=lambda k: k['code']):
-        print(entry["data"], end="")
+def read_message(path_file, total_bytes):
 
+    def create_dict(raw: str) -> list[dict[int:str]]:
 
-def create_dict(raw: str) -> list[dict[int:str]]:
+        obj: list[dict[int:str]] = []
 
-    obj: list[dict[int:str]] = []
+        for text in raw.split("|"):
+            obj.append({"code": int(text[1:3]), "data": text[5:-2]})
 
-    for text in raw.split("|"):
-        obj.append({"code": int(text[1:3]), "data": text[5:-2]})
+        return obj
 
-    return obj
+    def display_message(message: list[dict]) -> None:
 
+        for entry in sorted(message, key=lambda k: k['code']):
+            print(entry["data"], end="")
 
-def build_raw_text(raw: str) -> tuple[str, int]:
+    def build_raw_text(raw: str) -> tuple[str, int]:
 
-    append: int = 1
+        append: int = 1
 
-    data: str = ""
+        data: str = ""
 
-    for chr in list(raw):
+        for chr in list(raw):
 
-        data += chr
+            data += chr
 
-        if chr == "]":
-            data += "|"
-            append = 0
-            break
+            if chr == "]":
+                data += "|"
+                append = 0
+                break
 
-    return data, append
+        return data, append
 
+    def get_raw_text(path_file: str, total_bytes: int) -> str:
 
-def get_raw_text(path_file: str, total_bytes: int) -> str:
+        append: int = 0
 
-    append: int = 0
+        raw_text: str = ""
 
-    raw_text: str = ""
+        with open(path_file, 'r') as f:
 
-    with open(path_file, 'r') as f:
+            while raw_data := f.read(total_bytes):
 
-        while raw_data := f.read(total_bytes):
+                index = raw_data.find("[")
 
-            index = raw_data.find("[")
+                if index > -1 or append > 0:
 
-            if index > -1 or append > 0:
+                    text, append = build_raw_text(raw_data[index:]
+                                                  if index > -1 else raw_data)
+                    raw_text += text
 
-                text, append = build_raw_text(raw_data[index:]
-                                              if index > -1 else raw_data)
-                raw_text += text
+        return raw_text[:-1]
 
-    return raw_text[:-1]
+    return display_message(create_dict(get_raw_text(path_file, total_bytes)))
 
 
 def main() -> None:
@@ -60,7 +62,7 @@ def main() -> None:
 
         total_bytes: int = 15
 
-        display_message(create_dict(get_raw_text(path_file, total_bytes)))
+        read_message(path_file, total_bytes)
 
     except Exception as e:
 
